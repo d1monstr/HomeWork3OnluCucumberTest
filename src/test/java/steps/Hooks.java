44fb682d651d9a534d0e2059c7fd66fbf4e193b8
dmitry <dimon199689@gmail.com>
2020-09-28 14:41:03 +0300

    modified commit

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,6 +1,5 @@
 package steps;
 
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.qameta.allure.Allure;
@@ -15,16 +14,13 @@ import java.time.format.DateTimeFormatter;
 public class Hooks {
 
     @Before
-    public void beforeEach(){
+    public void before(){
         InitManager.initFramework();
     }
-
-
     @After
-    public void afterEach() {
+    public void after(){
         byte[] screenShot = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BYTES);
         Allure.getLifecycle().addAttachment(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-yy_hh:mm:ss")), "image/png", "png", screenShot);
         InitManager.quitFramework();
     }
-
 }
